412b0bb0be13d1b67c9fda4464f291ac3f603e95
jessica <jessicadhamala20@gmail.com>
2025-08-02 00:49:40 +0545

    addressmodel

--- app/src/main/java/com/example/petalsandbloom/model/AddressModel.kt
+++ app/src/main/java/com/example/petalsandbloom/model/AddressModel.kt
@@ -4,6 +4,7 @@ data class AddressModel(
     val id: String = "",
     val userId: String = "",
     val street: String = "",
+
     val city: String = "",
     val district: String = "",
     val postalCode: String = ""
