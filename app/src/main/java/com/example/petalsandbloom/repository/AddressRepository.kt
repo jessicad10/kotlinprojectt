4202fa976cebdd5eb8c6e3d20276a30542c8d0a5
jessica <jessicadhamala20@gmail.com>
2025-08-02 00:54:36 +0545

    address repo

--- app/src/main/java/com/example/petalsandbloom/repository/AddressRepository.kt
+++ app/src/main/java/com/example/petalsandbloom/repository/AddressRepository.kt
@@ -5,6 +5,7 @@ package com.example.petalsandbloom.repository
 import com.example.petalsandbloom.model.AddressModel
 
 interface AddressRepository {
+
     fun addAddress(address: AddressModel, callback: (Boolean, String) -> Unit)
     fun getAddresses(userId: String, callback: (List<AddressModel>) -> Unit)
     fun updateAddress(address: AddressModel, callback: (Boolean, String) -> Unit)
